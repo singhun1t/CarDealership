450886601b5a2d6a843b8e721a43a6201f609881
singhun1t <manvirp123@gmail.com>
2024-05-14 14:54:21 -0400

    phase 2 completed

--- src/main/java/com/pluralsight/DealershipFileManager.java
+++ src/main/java/com/pluralsight/DealershipFileManager.java
@@ -1,4 +1,47 @@
 package com.pluralsight;
 
+
+
+import java.io.BufferedReader;
+
+import java.io.FileReader;
+import java.io.IOException;
+
 public class DealershipFileManager {
+    String file = "dealership.csv";
+    Dealership dealership = null;
+
+    public void getDealership() {
+        String line;
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            while ((line = bufferedReader.readLine()) != null) {
+                String [] parts = line.split("\\|");
+                if(parts.length ==3){
+                    String businessName = parts[0];
+                    String address = parts[1];
+                    String phoneNumber = parts[2];
+                    Dealership dealershipInfo = new Dealership(businessName,address,phoneNumber);
+                } else if (parts.length ==8){
+                    int vin = Integer.parseInt(parts[0]);
+                    int year = Integer.parseInt(parts[1]);
+                    String make = parts[2];
+                    String model = parts[3];
+                    String vehicleType = parts[4];
+                    String color = parts[5];
+                    int odometer = Integer.parseInt(parts[6]);
+                    double price = Double.parseDouble(parts[7]);
+                    Vehicle vehicleInfo = new Vehicle(vin,year,make,model,vehicleType,color,odometer,price);
+                    dealership.addVehicle(vehicleInfo);
+                }
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void saveDealership(){
+        
+    }
 }
+
